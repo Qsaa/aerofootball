517ebc91d95c32ef62a32960b09a49d19db788fb
Alexander_laptop <ya-alexander@outlook.com>
2025-05-08 18:39:22 +0300

    added physics of the ball

--- main.cpp
+++ main.cpp
@@ -14,10 +14,65 @@
 #include <SDL3_image/SDL_image.h>
 #include <SDL3/SDL_main.h>
 
+#include <tuple>
+
+class Ball;
+
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
 static SDL_Texture* field_texture = nullptr;
 static SDL_Texture* ball_texture = nullptr;
+static Ball* ball = nullptr;
+
+
+class Ball
+{
+public:
+    Ball() : x_(0), y_(0), d_(100), vx_(5), vy_(5)
+    {};
+
+    float getX()
+    {
+        return x_;
+    }
+    float getY()
+    {
+        return y_;
+    }
+    float getD()
+    {
+        return d_;
+    }
+
+
+    std::tuple<float, float> wanna_go_to(float t = 0.2)
+    {
+        return {vx_* t + x_, vy_ * t + y_ };
+    }
+
+    void set_new_position(float x, float y)
+    {
+        x_ = x;
+        y_ = y;
+    }
+
+    void change_x_speed()
+    {
+        vx_ *= -1;
+    }
+    void change_y_speed()
+    {
+        vy_ *= -1;
+    }
+
+private:
+    float x_;
+    float y_;
+    float d_;
+
+    float vx_;
+    float vy_;
+};
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
@@ -37,13 +92,15 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
     field_texture = SDL_CreateTextureFromSurface(renderer, png_surface);
     SDL_DestroySurface(png_surface);
     
-    SDL_Surface* ball = IMG_Load("../ball.png");
-    if (!ball)
+    SDL_Surface* ball_surface = IMG_Load("../ball.png");
+    if (!ball_surface)
     {
         return SDL_APP_FAILURE;
     }
-    ball_texture = SDL_CreateTextureFromSurface(renderer, ball);
-    SDL_DestroySurface(ball);   
+    ball_texture = SDL_CreateTextureFromSurface(renderer, ball_surface);
+    SDL_DestroySurface(ball_surface); 
+
+    ball = new Ball{};
     
     return SDL_APP_CONTINUE;
 }
@@ -67,20 +124,37 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     const char* message = "Aerofootball";
     int w = 0, h = 0;
     float x, y;
-    const float scale = 4.0f;
+    const float scale = 1.0f;
 
     /* Center the message and scale it up */
     SDL_GetRenderOutputSize(renderer, &w, &h);
     SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+    x = (w / scale) / 2;
+    y = (h / scale) / 2;
 
     /* Draw the message */
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     SDL_RenderTexture(renderer, field_texture, nullptr, nullptr);
-    SDL_FRect rect = SDL_FRect{ x, y, 15, 15 };
+
+    float new_x;
+    float new_y;
+
+    std::tie(new_x, new_y) = ball->wanna_go_to();
+    // working engine start
+    if ((new_x + ball->getD()) > w || new_x < 0)
+    {
+        ball->change_x_speed();
+    }
+    if ((new_y + ball->getD()) > h || new_y < 0)
+    {
+        ball->change_y_speed();
+    }
+    // working engine finish
+    ball->set_new_position(new_x, new_y);
+
+    SDL_FRect rect = SDL_FRect{ ball->getX(), ball->getY(), ball->getD(), ball->getD() };
     SDL_RenderTexture(renderer, ball_texture, nullptr, &rect);
 
     //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
